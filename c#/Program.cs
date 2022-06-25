af805ee86aaee0cabfe0a7a2ce56e158a8d79731
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2022-06-25 09:50:47 -0300

    Create Program.cs
    codigo simples de ler idade

---
+++ c#/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Digite seu nome: ");
+String yourName = Console.ReadLine();
+Console.WriteLine($"Iae {yourName}!");
+Console.Write("Digite o seu ano de nascimento: ");
+int year = int.Parse(Console.ReadLine());
+int age = 2022 - year;
+Console.WriteLine($"Você tem {age} anos de idade!");
+
+if(age >= 18)
+{
+    Console.WriteLine("Você e maior de idade!");
+} 
+else
+{ 
+    Console.WriteLine("Você e menor de idade seu corno");
+}    
+  
+    
